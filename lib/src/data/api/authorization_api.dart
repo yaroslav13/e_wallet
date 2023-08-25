30fd0ea761d695a014e4f250216ef2eb0a069a79
Yaroslav Halivets <yarikgalivetc@gmail.com>
2023-08-25 15:59:29 +0200

    EW-5 delete unnecessary annotations of injectable

--- lib/src/data/api/authorization_api.dart
+++ lib/src/data/api/authorization_api.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:injectable/injectable.dart';
 
-@injectable
 final class AuthorizationApi {
   AuthorizationApi(this._firebaseAuth);
 
--- lib/src/domain/business_domain/interactors/test_interactor.dart
+++ lib/src/domain/business_domain/interactors/test_interactor.dart
@@ -1,10 +1,8 @@
 import 'package:e_wallet/src/domain/base/interactor.dart';
 import 'package:e_wallet/src/domain/business_domain/repositories/test_repositories.dart';
-import 'package:injectable/injectable.dart';
 
-@injectable
 final class TestInteractor implements Interactor<int, int> {
-  TestInteractor(this._repository);
+  const TestInteractor(this._repository);
 
   final TestRepository _repository;
 
