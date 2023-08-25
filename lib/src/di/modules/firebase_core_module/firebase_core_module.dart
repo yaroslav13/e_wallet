0f4d4bcebfec5338ffdc12f1876d00335bd26901
Yaroslav Halivets <yarikgalivetc@gmail.com>
2023-08-25 18:03:59 +0200

    EW-7 Firebase version refresh

--- lib/src/di/modules/firebase_core_module/firebase_core_module.dart
+++ lib/src/di/modules/firebase_core_module/firebase_core_module.dart
@@ -3,7 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:injectable/injectable.dart';
 
 @module
-abstract base class FirebaseCoreModule {
+abstract class FirebaseCoreModule {
   @preResolve
   Future<FirebaseApp> get firebase => Firebase.initializeApp(
         options: DefaultFirebaseOptions.currentPlatform,
