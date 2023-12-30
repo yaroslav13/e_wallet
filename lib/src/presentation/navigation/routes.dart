278b3e394145449051aea280a481542dec2e894c
Yaroslav Halivets <yaroslav.halivets@nixsolutions.com>
2023-12-31 00:17:20 +0100

    EW-38 Implement Rive animation on auth screen

--- lib/src/presentation/navigation/routes.dart
+++ lib/src/presentation/navigation/routes.dart
@@ -4,8 +4,17 @@ abstract final class Routes {
   Routes._();
 
   static const welcome = _WelcomeRoute();
+  static const authorization = _AuthorizationRoute();
 }
 
 final class _WelcomeRoute extends NamedRoute {
   const _WelcomeRoute() : super(pathFragment: '/', name: 'welcome');
+}
+
+final class _AuthorizationRoute extends NamedRoute {
+  const _AuthorizationRoute()
+      : super(
+          pathFragment: '/authorization',
+          name: 'authorization',
+        );
 }
